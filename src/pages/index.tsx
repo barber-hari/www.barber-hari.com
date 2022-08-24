1578fe59278767c8179b1c4ceb4cad0f3f7fe04e
jinsol <smokerjs.dev@gmail.com>
2022-08-24 21:31:33 +0900

    프리티어 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Background from 'components/home/Background';
 import Contents from 'components/home/Contents';
 import styled from 'styled-components';
 
- const Container = styled.div`
+const Container = styled.div`
   display: block;
   width: 100%;
   height: 100vh;
