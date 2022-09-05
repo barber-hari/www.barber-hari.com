897e814b7e5e5256df05059dd5e3fd4e1bf003ab
wumeenna <minu960604@gmail.com>
2022-09-05 18:00:20 +0900

    useScroll intersectionObserver 삭제

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,16 +1,8 @@
 import { useEffect, useState } from 'react';
 
-interface UseScrollProps {
-  root?: null;
-  rootMargin?: string;
-  threshold?: number;
-  onIntersect: IntersectionObserverCallback;
-}
-
 export const useScroll = () => {
   const [scrollHeight, setScrollHeight] = useState(0);
   const target = document.querySelector('main');
-  const option = { threshold: 0.2 };
 
   const scrollHandler = () => {
     setScrollHeight(target?.scrollTop as number);
@@ -21,9 +13,6 @@ export const useScroll = () => {
   }, [scrollHeight]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(scrollHandler, option);
-    observer.observe(target as HTMLElement);
-
     const catchScroll = () => {
       target?.addEventListener('scroll', scrollHandler);
     };
